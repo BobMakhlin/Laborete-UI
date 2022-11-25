abf91145147662b362f788fd421ac859e18fae26
BobMakhlin <borismakhlin5@gmail.com>
2022-11-25 23:40:28 +0200

    RETE-2: Add error handling

--- src/App.js
+++ src/App.js
@@ -4,30 +4,49 @@ import "./App.scss";
 function App() {
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const fetchUser = async () => {
     setIsLoading(true);
+    setError(null);
 
     const response = await fetch(
       "http://localhost:8080/v1/users/179a44f9-7032-47c8-ac07-180a93159ce0"
     );
-    const user = await response.json();
 
-    console.log("data:", user);
+    if (!response.ok) {
+      setError("Something went wrong!!");
+      setIsLoading(false);
+      return;
+    }
+
+    const user = await response.json();
     setUser(user);
     setIsLoading(false);
   };
 
+  let content = <p>User was not found</p>;
+
+  if (user) {
+    content = (
+      <p>
+        {user.firstName} {user.secondName}
+      </p>
+    );
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>The data is being loaded</p>;
+  }
+
   return (
     <div className="App">
       <button onClick={fetchUser}>Fetch</button>
-      {isLoading && <p>The data is being loaded</p>}
-      {!isLoading && user && (
-        <p>
-          {user.firstName} {user.secondName}
-        </p>
-      )}
-      {!isLoading && !user && <p>User was not found</p>}
+      {content}
     </div>
   );
 }
