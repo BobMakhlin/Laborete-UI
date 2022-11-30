c3567a3bcdbe01757bb140d75a15bae81821b7f6
BobMakhlin <borismakhlin5@gmail.com>
2022-11-30 22:09:29 +0200

    Display error

--- src/components/User/MyProfile.js
+++ src/components/User/MyProfile.js
@@ -2,6 +2,8 @@ import { Card, LinearProgress } from "@mui/material";
 import { useCallback, useEffect, useState } from "react";
 import styles from "./MyProfile.module.scss";
 
+const SERVER_ERROR_MESSAGE = "An error occured on our side";
+
 export const MyProfile = (props) => {
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
@@ -11,19 +13,23 @@ export const MyProfile = (props) => {
     setIsLoading(true);
     setError(null);
 
-    const response = await fetch(
-      "http://localhost:8080/v1/users/179a44f9-7032-47c8-ac07-180a93159ce0"
-    );
+    try {
+      const response = await fetch(
+        "http://localhost:8080/v1/users/179a44f9-7032-47c8-ac07-180a93159ce0"
+      );
 
-    if (!response.ok) {
-      setError("Something went wrong!!");
+      if (!response.ok) {
+        setError(response.statusText);
+      } else {
+        const user = await response.json();
+        setUser(user);
+      }
+    } catch (e) {
+      console.log("error:", e);
+      setError(SERVER_ERROR_MESSAGE);
+    } finally {
       setIsLoading(false);
-      return;
     }
-
-    const user = await response.json();
-    setUser(user);
-    setIsLoading(false);
   }, []);
 
   useEffect(() => {
@@ -31,16 +37,24 @@ export const MyProfile = (props) => {
   }, [fetchUser]);
 
   if (isLoading) {
-    return <div className={styles.loadingBlock}>
-      <LinearProgress className={styles.loadingProgress} />
-    </div>
+    return (
+      <div className={styles.loadingBlock}>
+        <LinearProgress className={styles.loadingProgress} />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div>{error}</div>;
   }
 
   if (user) {
     return (
       <>
         <Card className={styles.header}>
-          <h2>{user.firstName} {user.secondName}</h2>
+          <h2>
+            {user.firstName} {user.secondName}
+          </h2>
           <h3>{user.position}</h3>
           <p className={styles.location}>{user.location}</p>
         </Card>
