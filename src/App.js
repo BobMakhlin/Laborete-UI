c63d0f0c6b73105e1ec2f701f706a1c820158c7c
BobMakhlin <borismakhlin5@gmail.com>
2023-01-10 22:33:35 +0200

    RETE-2: Add a header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import "./App.scss";
 import { MyProfile } from "./components/User/MyProfile";
-import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
+import { Root } from "./components/Root/Root";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <MyProfile />,
-  },
-  {
-    path: "/my-profile",
-    element: <MyProfile />,
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <MyProfile />,
+      },
+      {
+        path: "my-profile",
+        element: <MyProfile />,
+      },
+    ],
   },
 ]);
 
 function App() {
   return (
     <div className="App">
-      <RouterProvider router={router}></RouterProvider>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/components/Root/Root.js
@@ -0,0 +1,36 @@
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import { Box } from "@mui/system";
+import { NavLink, Outlet } from "react-router-dom";
+import styles from "./Root.module.css";
+
+export const Root = (props) => {
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" color="primary">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              LABORETE
+            </Typography>
+
+            <Box className={styles.pageButtons} sx={{ display: "flex" }}>
+              <NavLink to={"/companies"}>
+                <Button sx={{ my: 2, color: "white", display: "block" }}>
+                  Companies
+                </Button>
+              </NavLink>
+
+              <NavLink to={"/my-profile"}>
+                <Button sx={{ my: 2, color: "white", display: "block" }}>
+                  My profile
+                </Button>
+              </NavLink>
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
+
+      <Outlet />
+    </>
+  );
+};
